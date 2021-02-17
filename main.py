e5a12649d266162d956bf24963283eddd0e71033
SolomiyaSmoliar <solomiia.smoliar@ucu.edu.ua>
2021-02-17 12:15:53 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,52 +1,82 @@
-# import folium
-# file = 'C:/Users/Solomiya/Desktop/list.csv'
-# lat = data['lat']
-# lon = data['lon']
-# m = folium.Map([48.314775, 25.082925],
-#                 zoom_start=10)
-# fg = folium.FeatureGroup(name='kosiv')
+import folium
+from geopy.geocoders import Nominatim
+from haversine import haversine
+import math
+geolocator = Nominatim(user_agent='Location')
 
-# for lt, ln in zip(lat, lon):
-#     fg.add_child(folium.Marker(location=[lt,ln],
-#                                 popup='1900',
-#                                 icon = folium.Icon()))
-# m.add_child(fg)
-# m.save('C:/Users/Solomiya/Documents/Visual Studio/location/index.html')
+tooltip = 'Click fot more info'
 
-def read_data(file:str) -> list:
+
+def read_data(file: str) -> list:
     new_list = []
-    file_ = open(file, 'r' )
+    file_ = open(file, 'r')
     contents = file_.readlines()
     for line in contents[14:]:
         line_1 = []
         line = line.strip()
         line = line.split(',')
-        first_line = line[0].replace('\t','')
-        first_line = first_line.replace(') ','.')
-        first_line = first_line.replace(')','.')
-        first_line = first_line.replace('(','.')
-        first_line = first_line.replace('}','.')
-        first_line = first_line.replace('{','.')
+        first_line = line[0].replace('\t', '')
+        first_line = first_line.replace(') ', '.')
+        first_line = first_line.replace(')', '.')
+        first_line = first_line.replace('(', '.')
+        first_line = first_line.replace('}', '.')
+        first_line = first_line.replace('{', '.')
         first_line = first_line.split('.')
         first_line[0] = first_line[0].replace('#', '')
         first_line[0] = first_line[0].replace(' ', '')
         line_1.append(first_line[0])
         line_1.append(int(first_line[1]))
-        line_1.append(first_line[-1])
+        line_1.append(line[1])
         new_list.append(line_1)
     return new_list
 
-def similar_year_film(year:int, film_list:list) -> list:
+
+def similar_year_film(year: int, film_list: list) -> list:
     similar_year = []
     for i in film_list:
-        if year in i:
+        if year in i and i not in similar_year:
             similar_year.append(i)
     return similar_year
 
-def lenght_calculeting(latitude:float, longitude:float, film_list:list) -> list:
+
+def lenght_calculeting(latitude: float, longitude: float, film_list: list) -> list:
+    location = (latitude, longitude)
+    for i in film_list:
+        location_2 = coordinats(i[-1])
+        lenght = haversine(location, location_2)
+        i.append(location_2)
+        i.append(lenght)
+    film_list.sort(key=lambda x: x[-1])
+    return film_list[:10]
 
 
-if __name__ == '__main__':
-    file = 'C:/Users/Solomiya/Desktop/list.list'
-    film_list = read_data(file)
-    print(similar_year_film(2014, film_list))
+def coordinats(city: str) -> list:
+    location = geolocator.geocode(city)
+    return location.latitude, location.longitude
+
+
+def map_generator(latitude: float, longitude: float, year: int, file_: str):
+    colors = ['red', 'blue', 'purple', 'orange', 'darkblue',
+              'darkgreen', 'cadetblue', 'darkpurple', 'pink', 'lightblue']
+    film_list = read_data(file_)
+    similar = similar_year_film(year, film_list)
+    result = lenght_calculeting(latitude, longitude, similar)
+    m = folium.Map()
+
+    first_layear = folium.FeatureGroup(name='first_layear')
+
+    first_layear.add_child(folium.Marker(location=[49.83826, 24.02324],
+                                         popup='I am hear',
+                                         tooltip=tooltip,
+                                         icon=folium.Icon(color='green', icon="home", prefix='fa')))
+
+    for i in range(len(result)):
+        col = colors[i]
+        lt, ln = result[i][3]
+        first_layear.add_child(folium.Marker(location=[lt, ln],
+                                             popup=result[i][0],
+                                             tooltip=tooltip,
+                                             icon=folium.Icon(color=col, icon='cloud')))
+    m.add_child(first_layear)
+    m.save('C:/Users/Solomiya/Documents/Visual Studio/location/index.html')
+    return result
